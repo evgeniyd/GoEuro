f6bf00da534fc8a05e8f8616b3226d29264eed6f
Evgeniy Dubinin <thisiskilling@me.com>
2016-10-23 13:40:36 +0300

    Initial commit (GoEuroKit)

---
+++ GoEuroKit/GoEuroKit/GoEuroKit.h
@@ -0,0 +1,19 @@
+//
+//  GoEuroKit.h
+//  GoEuroKit
+//
+//  Created by Evgen Dubinin on 10/23/16.
+//  Copyright © 2016 GoEuro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GoEuroKit.
+FOUNDATION_EXPORT double GoEuroKitVersionNumber;
+
+//! Project version string for GoEuroKit.
+FOUNDATION_EXPORT const unsigned char GoEuroKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoEuroKit/PublicHeader.h>
+
+
