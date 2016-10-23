709226072ede9d0ab90d1ca3fe03382ba4b6f124
Evgeniy Dubinin <thisiskilling@me.com>
2016-10-23 18:05:34 +0300

    Add GoEuro-iOS project

---
+++ GoEuro-iOS/GoEuro-iOSTests/GoEuro_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  GoEuro_iOSTests.swift
+//  GoEuro-iOSTests
+//
+//  Created by Evgen Dubinin on 10/23/16.
+//  Copyright © 2016 GoEuro. All rights reserved.
+//
+
+import XCTest
+@testable import GoEuro_iOS
+
+class GoEuro_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
